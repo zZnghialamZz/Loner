b58555653852854b21630b8215596ae95619922a
Nghia Lam <nghialam12795@gmail.com>
2022-12-16 09:55:43 +0700

    Remove all current game code

--- Source/Loner/Private/Utilities/Logging.cpp
+++
@@ -1,7 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Utilities/Logging.h"
-
-DEFINE_LOG_CATEGORY(LogLoner);
-DEFINE_LOG_CATEGORY(LogLonerLevelGenerator);
