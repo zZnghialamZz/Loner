647790d1c68caa7880571c0ade1904021f7c5a6c
Nghia Lam <nghialam12795@gmail.com>
2022-12-21 20:22:04 +0700

    Base Attribute Component setup

---
+++ Source/TheLoner/Private/Components/Attributes/TLBaseAttributeComponent.cpp
@@ -0,0 +1,73 @@
+// @ The Loner Game, Nghia Lam.All Right Reserved
+
+#include "Components/Attributes/TLBaseAttributeComponent.h"
+
+UTLBaseAttributeComponent::UTLBaseAttributeComponent()
+	: BaseValue(0.0f)
+{
+}
+
+// Sets default values for this component's properties
+UTLBaseAttributeComponent::UTLBaseAttributeComponent(const float BaseValue)
+	: BaseValue(BaseValue)
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UTLBaseAttributeComponent::AddModifier(const FTLAttributeStatsModifier& Modifier)
+{
+	IsDirty = true;
+	ListModifiers.Add(Modifier);
+	ListModifiers.Sort(
+		[](const FTLAttributeStatsModifier& m1, const FTLAttributeStatsModifier& m2)
+		{
+			return m1.Order < m2.Order;
+		});
+}
+
+void UTLBaseAttributeComponent::RemoveModifier(const FTLAttributeStatsModifier& Modifier)
+{
+	IsDirty = true;
+	ListModifiers.RemoveSwap(Modifier, false);
+}
+
+float UTLBaseAttributeComponent::CalculateCurrentValue()
+{
+	float Result = BaseValue;
+
+	float SumPercentAdd = 0;
+	for (int i = 0; i < ListModifiers.Num(); i++)
+	{
+		switch (ListModifiers[i].Type)
+		{
+			case EStatModifierType::Flat:
+			{
+				Result += ListModifiers[i].Value;
+				break;
+			}
+			case EStatModifierType::PercentAdd:
+			{
+				SumPercentAdd += ListModifiers[i].Value;
+
+				// If we're at the end of the list OR the next modifer isn't of this type (We did the sort when adding modifier).
+				if (i + 1 >= ListModifiers.Num() || ListModifiers[i + 1].Type != EStatModifierType::PercentAdd)
+				{
+					Result *= (1 + SumPercentAdd);
+					SumPercentAdd = 0;
+				}
+
+				break;
+			}
+			case EStatModifierType::PercentMultiply:
+			{
+				Result *= (1 + ListModifiers[i].Value);
+				break;
+			}
+			default: break;
+		}
+	}
+
+	return Result;
+}
---
+++ Source/TheLoner/Public/Components/Attributes/TLBaseAttributeComponent.h
@@ -0,0 +1,121 @@
+// @ The Loner Game, Nghia Lam.All Right Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "TLBaseAttributeComponent.generated.h"
+
+UENUM()
+enum class EStatModifierType : uint8
+{
+	Flat,
+	PercentAdd,
+	PercentMultiply,
+};
+
+USTRUCT(BlueprintType, Blueprintable)
+struct FTLAttributeStatsModifier
+{
+	GENERATED_BODY()
+
+	UPROPERTY(VisibleAnywhere, Category = "Attribute Stats Modifier")
+	float Value;
+
+	UPROPERTY(VisibleAnywhere, Category = "Attribute Stats Modifier")
+	int Order;
+
+	UPROPERTY(VisibleAnywhere, Category = "Attribute Stats Modifier")
+	EStatModifierType Type;
+
+	UPROPERTY(VisibleAnywhere, Category = "Attribute Stats Modifier")
+	TObjectPtr<UObject> Source;
+
+public:
+
+	FTLAttributeStatsModifier()
+		: Value(0.0f)
+		, Order(0)
+		, Type(EStatModifierType::Flat)
+		, Source(nullptr)
+	{
+	}
+
+	FTLAttributeStatsModifier(float Value, int Order, EStatModifierType Type, TObjectPtr<UObject> FromSource)
+		: Value(Value)
+		, Order(Order)
+		, Type(Type)
+		, Source(FromSource)
+	{
+	}
+
+	FTLAttributeStatsModifier(float Value, EStatModifierType Type)
+		: Value(Value)
+		, Order((int)Type)
+		, Type(Type)
+		, Source(nullptr)
+	{
+	}
+
+	FTLAttributeStatsModifier(float Value, EStatModifierType Type, TObjectPtr<UObject> FromSource)
+		: Value(Value)
+		, Order((int)Type)
+		, Type(Type)
+		, Source(FromSource)
+	{
+	}
+
+	bool operator==(const FTLAttributeStatsModifier& Other) const
+	{
+		return Value == Other.Value && Order == Other.Order && Type == Other.Type && Source == Other.Source;
+	}
+};
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class THELONER_API UTLBaseAttributeComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+
+	// Sets default values for this component's properties
+	UTLBaseAttributeComponent();
+	UTLBaseAttributeComponent(const float BaseValue);
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "Attributes Component")
+	FORCEINLINE float GetValue()
+	{
+		if (!IsDirty && LastBaseValue == BaseValue) return CurrentValue;
+
+		IsDirty = false;
+		LastBaseValue = BaseValue;
+		CurrentValue = CalculateCurrentValue();
+
+		return CurrentValue;
+	}
+
+	UFUNCTION(BlueprintCallable, Category = "Attributes Component")
+	virtual void AddModifier(const FTLAttributeStatsModifier& Modifier);
+
+	UFUNCTION(BlueprintCallable, Category = "Attributes Component")
+	virtual void RemoveModifier(const FTLAttributeStatsModifier& Modifier);
+
+	UFUNCTION(BlueprintCallable, Category = "Attributes Component")
+	virtual float CalculateCurrentValue();
+
+protected:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Attributes Component")
+	TArray<FTLAttributeStatsModifier> ListModifiers;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Attributes Component")
+	float BaseValue;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Attributes Component")
+	float CurrentValue;
+
+	bool IsDirty = true;
+	float LastBaseValue = FLT_MIN;
+};
